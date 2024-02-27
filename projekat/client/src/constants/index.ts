8245d68078783479dfa305aca03ab1ad84d3aeae
Dimitrije Ostojic <dimitrijeostojic01@gmail.com>
2024-02-27 10:35:57 +0100

    Kreirane konstante

---
+++ projekat/client/src/constants/index.ts
@@ -0,0 +1,28 @@
+//koriscene na dashboardu za status barove
+export const objectReferralsInfo = [
+    {
+      title: 'Social Media',
+      percentage: 64,
+      color: '#334257',
+    },
+    {
+      title: 'Marketplace',
+      percentage: 40,
+      color: '#334257',
+    },
+    {
+      title: 'Websites',
+      percentage: 50,
+      color: '#334257',
+    },
+    {
+      title: 'Digital Ads',
+      percentage: 80,
+      color: '#334257',
+    },
+    {
+      title: 'Others',
+      percentage: 15,
+      color: '#334257',
+    },
+  ];
