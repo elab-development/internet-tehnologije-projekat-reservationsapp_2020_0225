f8877631a99ea348fa5cd09962fb608be5cc2baf
Dimitrije Ostojic <dimitrijeostojic01@gmail.com>
2024-02-27 10:36:41 +0100

    Kreirana komponenta za menadzera i ekspotovane sve komponente

---
+++ projekat/client/src/components/index.ts
@@ -0,0 +1,26 @@
+
+
+// charts
+import PieChart from './charts/PieChart';
+import ObjectReferrals from './charts/ObjectReferrals';
+import TotalRevenue from './charts/TotalRevenue';
+import SatisfactionBox from './charts/SatisfactionBox';
+import NumberOfAvailableObjects from './charts/NumberOfAvailableObjects';
+import CustomButton from './common/CustomButton';
+import ObjectCard from './common/ObjectCard';
+import Profile from './common/Profile';
+import ManagerCard from './manager/ManagerCard';
+
+
+export {
+
+  PieChart,
+  ObjectReferrals,
+  TotalRevenue,
+  SatisfactionBox,
+  CustomButton,
+  ObjectCard,
+  Profile,
+  NumberOfAvailableObjects,
+  ManagerCard
+};
